7c8c569d56a87b907ca9683fcf972b42e1547fe4
dariocolic <dario.colic.ri@gmail.com>
2019-01-30 17:02:03 +0100

    logo added.

--- app/index.php
+++ app/index.php
@@ -18,7 +18,7 @@
 
   <body>
     <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">LOGO</a>
+      <a class="navbar-brand" href="index.php"><img style="width: 110px;" src="src/logo.png"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
